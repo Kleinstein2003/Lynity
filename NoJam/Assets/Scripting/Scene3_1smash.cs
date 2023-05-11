e479cd3b89c95dd7c7083ab6e73fed121925ad28
justinkoelmel <132935333+justinkoelmel@users.noreply.github.com>
2023-05-11 18:02:12 +0200

    Justin Sound Scene 3.1

--- NoJam/Assets/Scripting/Scene3_1smash.cs
+++ NoJam/Assets/Scripting/Scene3_1smash.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Scene3_1smash : MonoBehaviour
 {
     public GameObject SpiegelDead;
+    public AudioSource audioSource;
+    public AudioClip AudioClip;
 
     void OnMouseDown()
     {
+        audioSource.PlayOneShot(AudioClip);
         Destroy(gameObject);
         Instantiate(SpiegelDead, transform.position, Quaternion.identity);
     }
