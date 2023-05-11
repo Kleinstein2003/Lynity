c013f6f08b1153b652f543e860f879b3ddd96ad4
Tom Shi <132930347+Kleinstein2003@users.noreply.github.com>
2023-05-11 21:03:30 +0200

    hayat

---
+++ NoJam/Assets/Scripting/trial_Huhn.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class trial_Huhn : MonoBehaviour
+{
+    void Update()
+    {
+        if(transform.childCount<1)
+        {
+            SceneManager.LoadScene("S1_Küche");
+        }
+    }
+}
